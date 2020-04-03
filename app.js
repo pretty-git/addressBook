a3532ae40d20d7228762873acc3ddc0ff3f0454f
zqx <2654784320@qq.com>
2020-04-03 16:06:47 +0800

    addressbook

---
+++ app.js
@@ -0,0 +1,22 @@
+//app.js
+App({
+  // 查询绑定的数据
+  onLaunch() {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+        if (capsule) {
+          this.globalData.Custom = capsule;
+          this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+        } else {
+          this.globalData.CustomBar = e.statusBarHeight + 50;
+        }
+      }
+    })
+  },
+  globalData: {
+    CustomBar: '',
+    StatusBar: '',
+  }
+})
